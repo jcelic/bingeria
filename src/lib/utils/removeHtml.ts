bca9da39fa0b935daf752634a8a87a5b453af9a6
Josip Celic <josipcelic22@gmail.com>
2026-09-13 19:00:25 +0200

    fix: improve HTML removal

--- src/lib/utils/removeHtml.ts
+++ src/lib/utils/removeHtml.ts
@@ -1,3 +1,12 @@
 export const removeHtml = (html: string) => {
-  return html.replace(/<[^>]*>/g, '');
+  return html
+    .replace(/<br\s*\/?>/gi, ' ')
+    .replace(/<\/?(p|div|li)[^>]*>/gi, ' ')
+    .replace(/<[^>]*>/g, '')
+    .replace(/&nbsp;/g, ' ')
+    .replace(/&amp;/g, '&')
+    .replace(/&quot;/g, '"')
+    .replace(/&#39;/g, "'")
+    .replace(/\s+/g, ' ')
+    .trim();
 };
